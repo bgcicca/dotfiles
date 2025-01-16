5d65b3e46297f6c5e536a61185887314fbb94b92
BrunoCiccarino <landneco0@gmail.com>
2025-01-16 00:55:51 -0300

    🎨 Adding transparent background

--- .wezterm.lua
+++ .wezterm.lua
@@ -47,4 +47,33 @@ return {
     { key = 'C', mods = 'CTRL', action = act.CopyTo 'Clipboard' },
     { key = 'V', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
   },
+  
+  scrollback_lines = 10000,  
+  enable_scroll_bar = true,  
+
+  scroll_bar = {
+    width = "2",               
+    color = "#FFFFFF",   
+  },
+  window_background_opacity = 0.5,
+  win32_acrylic_accent_color = "rgb(94, 64, 157)",
+  webgpu_power_preference = "HighPerformance",
+  front_end = "OpenGL",
+  prefer_egl = true,
+  window_padding = {
+    left = 5,
+    right = 5,
+    top = 5,
+    bottom = 5,
+  },
+  hide_tab_bar_if_only_one_tab = true,
+  default_cursor_style = "BlinkingBar",
+  use_fancy_tab_bar = true,
+  tab_bar_at_bottom = false,
+  window_decorations = "NONE | RESIZE",
+  cell_width = 0.9,
+  window_frame = {
+    active_titlebar_bg = "transparent",
+    inactive_titlebar_bg = "#333333",
+  },
 }
